350f9d9eb98da6341da45a2867eaa75c4d6d5472
JHodgkins <jhodgkins78@gmail.com>
2022-04-11 02:19:09 +0000

    code: updated exmples

--- map.js
+++ map.js
@@ -5,16 +5,28 @@
  */
 
 // Using a for loop
-
+let nums = [1,2,3,4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log('for loop',results); // returns [2,4,6,8,10]
 
 // Using map()
-
+const multiplyByTwo = function(nums) {
+  return nums * 2;
+}
+const mapResults = nums.map(multiplyByTwo);
+console.log('using map', mapResults);
 
 // Simplified w/ map()
 
+const simplified = nums.map(function(nums){return nums * 2});
+console.log('simplified way:',simplified);
 
 // Simplfied w/ map() + arrow function
-
+const simplifiedWithArrow = nums.map(nums => nums * 2);
+console.log('arrow way:',simplifiedWithArrow);
 
 // With objects:
 const students = [
@@ -37,3 +49,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.id, student.name, student.profession]);
+console.log(studentsWithIds);
